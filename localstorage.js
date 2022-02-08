f338ee9be0f4a0914762726acea3670e7173a48a
inspectah14 <lambada_41@hotmail.com>
2022-02-08 12:43:18 +0100

    updated funcs on click

--- localstorage.js
+++ localstorage.js
@@ -2,41 +2,32 @@ const frontCard = document.querySelector("#framsida");
 const backCard = document.querySelector("#baksida");
 const submitBtn = document.querySelector("#submitBtn");
 
-// submitBtn.addEventListener("click", (e) => {
-//   e.preventDefault();
-
-//   if(frontCard.value === "" || backCard.value === "") {
-//     alert("Please complete the card.")
-//   } else {
-//     localStorage.setItem("card", JSON.stringify({
-//     frontside: frontCard.value,
-//     backside: backCard.value
-//     }));
-//     location.reload();
-//   }
-// })
-
-
-submitBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-
+const cardArr = () => {
   if(frontCard.value === "" || backCard.value === "") {
     alert("Please complete the card.")
   } else {
-    
+
     const cards = (() => {
       const cardValue = localStorage.getItem('card');
       return cardValue === null
         ? []
         : JSON.parse(cardValue);
     })();
-  
+
     cards.push({"frontside": frontCard.value, "backside": backCard.value});
-  
+
     localStorage.setItem('card', JSON.stringify(cards));
   }
+}
 
+const clearFields = () => {
   frontCard.value = "";
   backCard.value = "";
+};
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  cardArr();
+  clearFields();
 
 })
