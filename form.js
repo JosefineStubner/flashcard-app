8a366a35eebd5b395114eb13ff23af4c772c199c
inspectah14 <lambada_41@hotmail.com>
2022-02-08 14:11:46 +0100

    updated indentation on index.html

--- form.js
+++ form.js
@@ -4,10 +4,10 @@ sparaKnapp.addEventListener('click', function(e){ //lyssnar på click och callba
     e.preventDefault(); //Denna rad pga förhindra att sidan laddas om
     
     let frontText = document.getElementById('front'); // hämtar framsidns input
-    let framsidaKort = document.getElementById('framsida'); // hämtar diven som framsidans input ska in i.
+    let framsidaKort = document.getElementById('framsida1'); // hämtar diven som framsidans input ska in i.
 
     let bakText = document.getElementById('bak'); // hämtar baksidans input
-    let baksidaKort = document.getElementById('baksida'); // hämtar diven som baksidans input ska in i.
+    let baksidaKort = document.getElementById('baksida1'); // hämtar diven som baksidans input ska in i.
 
     framsidaKort.innerHTML = frontText.value; //skrier/sätter ut inputen med innerhtml av framsidans value
     baksidaKort.innerHTML = bakText.value; // skriver/sätter ut inputen med innerhtml av baksidans value
